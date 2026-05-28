a304a76665c527638a986bb3a90f5a4e99ea889d
Zhengxu SUN <zhengxu.sun@cqumail.com>
2026-05-28 14:22:39 +1000

    Initial commit

---
+++ healthapp/bmiCalculator.ts
@@ -0,0 +1,14 @@
+const calculateBMI = (height: number, weight: number): string => {
+  const bmi = weight / (height / 100) ** 2;
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi < 25) {
+    return "Normal range";
+  } else if (bmi < 30) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+};
+
+console.log(calculateBMI(180, 74));
---
+++ healthapp/exerciseCalculator.ts
@@ -0,0 +1,38 @@
+interface ExerciseResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+const calculateExercises = (
+  dailyhours: number[],
+  target: number,
+): ExerciseResult => {
+  const periodLength = dailyhours.length;
+  const trainingDays = dailyhours.filter((h) => h > 0).length;
+
+  const sum = dailyhours.reduce((acc, cur) => acc + cur, 0);
+  const average = sum / periodLength;
+  const success = average >= target;
+  const rating = success ? 3 : average >= target / 2 ? 2 : 1;
+  const ratingDescription = success
+    ? "Great job!"
+    : average >= target / 2
+      ? "Not too bad but could be better"
+      : "You need to work harder";
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
